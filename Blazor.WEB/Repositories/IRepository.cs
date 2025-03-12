5e0d5a092b2ce2493f8f6a42931d345090a84bc4
AlexisOE <21030319@itesa.edu.mx>
2025-03-11 18:48:37 -0600

    Versión_04: Creación de los componentes Post, Get en la API y en la WEB.

---
+++ Blazor.WEB/Repositories/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Blazor.WEB.Repositories
+{
+    public interface IRepository
+    {
+        Task<HttpResponseWraper<T>> Get<T>(string url);
+        Task<HttpResponseWraper<object>> Post<T>(string url, T model);
+        Task<HttpResponseWraper<TResponse>> Post<T, TResponse>(string url, T model);
+    }
+}
---
+++ Blazor.WEB/Repositories/Repository.cs
@@ -0,0 +1,154 @@
+﻿
+using System.Text.Json;
+using System.Text;
+
+namespace Blazor.WEB.Repositories
+{
+    public class Repository : IRepository
+    {
+        private readonly HttpClient _httpClient;
+
+
+
+        private JsonSerializerOptions _jsonDefaultOptions => new JsonSerializerOptions
+        {
+
+
+
+                PropertyNameCaseInsensitive = true,
+
+
+        };
+
+
+
+
+
+        public Repository(HttpClient httpClient)
+        {
+
+
+            _httpClient = httpClient;
+
+
+        }
+
+
+
+
+
+        public async Task<HttpResponseWraper<T>> Get<T>(string url)
+        {
+
+
+            var responseHttp = await _httpClient.GetAsync(url);
+
+
+            if (responseHttp.IsSuccessStatusCode)
+            {
+
+
+                var response = await UnserializeAnswer<T>(responseHttp, _jsonDefaultOptions);
+
+
+                return new HttpResponseWraper<T>(response, false, responseHttp);
+
+
+            }
+
+
+
+
+
+            return new HttpResponseWraper<T>(default, true, responseHttp);
+
+
+        }
+
+
+
+
+
+        public async Task<HttpResponseWraper<object>> Post<T>(string url, T model)
+        {
+
+
+            var mesageJSON = JsonSerializer.Serialize(model);
+
+
+            var messageContet = new StringContent(mesageJSON, Encoding.UTF8, "application/json");
+
+
+            var responseHttp = await _httpClient.PostAsync(url, messageContet);
+
+
+            return new HttpResponseWraper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);
+
+
+        }
+
+
+
+
+
+        public async Task<HttpResponseWraper<TResponse>> Post<T, TResponse>(string url, T model)
+        {
+
+
+            var messageJSON = JsonSerializer.Serialize(model);
+
+
+            var messageContet = new StringContent(messageJSON, Encoding.UTF8, "application/json");
+
+
+            var responseHttp = await _httpClient.PostAsync(url, messageContet);
+
+
+            if (responseHttp.IsSuccessStatusCode)
+            {
+
+
+                var response = await UnserializeAnswer<TResponse>(responseHttp, _jsonDefaultOptions);
+
+
+                return new HttpResponseWraper<TResponse>(response, false, responseHttp);
+
+
+            }
+
+
+            return new  HttpResponseWraper<TResponse>(default, !responseHttp.IsSuccessStatusCode, responseHttp);
+
+
+        }
+
+
+
+        private async Task<T> UnserializeAnswer<T>(HttpResponseMessage httpResponse, JsonSerializerOptions jsonSerializerOptions)
+        {
+
+
+            var respuestaString = await httpResponse.Content.ReadAsStringAsync();
+
+
+            return JsonSerializer.Deserialize<T>(respuestaString, jsonSerializerOptions)!;
+
+
+        }
+
+        Task<HttpResponseWraper<T>> IRepository.Get<T>(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<HttpResponseWraper<object>> IRepository.Post<T>(string url, T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<HttpResponseWraper<TResponse>> IRepository.Post<T, TResponse>(string url, T model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
